7a8f2db5bbcff592d54980481d79f0c53eb300e7
fenmow <lucas170105@outlook.com>
2023-12-10 01:03:19 -0300

    Added contact page and contactForm component

--- src/components/ContactCard/ContactCard.tsx
+++ src/components/ContactCard/ContactCard.tsx
@@ -1,15 +1,31 @@
-import { useEffect, useRef } from "react"
+import { useRef, useState } from "react"
 import Style from "./ContactCard.module.scss"
 import BgHero from "public/bg-pattern-hero-home.svg"
 
 const ContactCard: React.FC = () => {
-  const nameRef = useRef<HTMLInputElement>(null)
+  const [nameMessage, setNameMessage] = useState<boolean>(false)
+  const nameRef = useRef<HTMLInputElement>({} as HTMLInputElement)
+  const [emailMessage, setEmailMessage] = useState<boolean>(false)
+  const emailRef = useRef<HTMLInputElement>({} as HTMLInputElement)
+  const [phoneMessage, setPhoneMessage] = useState<boolean>(false)
+  const phoneRef = useRef<HTMLInputElement>({} as HTMLInputElement)
+  const messageRef = useRef<HTMLTextAreaElement>({} as HTMLTextAreaElement)
 
   
   const handleSubmit = (ev: { preventDefault: () => void }) => {
     ev.preventDefault()
-    if (nameRef.current?.value === '' ) {
-      console.log('teste')
+    setNameMessage(false), setEmailMessage(false), setPhoneMessage(false)
+    if (nameRef.current?.value.length === 0 ) {
+      setNameMessage(true)
+    } else if (emailRef.current?.value.length === 0) {
+      setEmailMessage(true)
+    } else if (phoneRef.current?.value.length === 0) {
+      setPhoneMessage(true)
+    } else {
+      nameRef.current.value = ''
+      emailRef.current.value = ''
+      phoneRef.current.value = ''
+      messageRef.current.value = ''
     }
   }
 
@@ -20,10 +36,19 @@ const ContactCard: React.FC = () => {
         <p>Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.</p>
       </div>
       <form className={Style.contact_form} onSubmit={handleSubmit}>
-        <input ref={nameRef} type="text" name="name" id="name" className={Style.form_input} placeholder="Name"/>
-        <input type="email" name="email" id="email" className={Style.form_input} placeholder="Email Address" required />
-        <input type="tel" name="tel" id="tel" className={Style.form_input} placeholder="Phone" required/>
-        <textarea name="message" id="message" cols={40} rows={4} placeholder="Your Message"></textarea>
+        <div className={`${Style.container} ${nameMessage === true ? `${Style.show}` : ''}`}>
+          <input ref={nameRef} type="text" name="name" id="name" className={Style.form_input} placeholder="Name"/>
+          <span>Can´t be empty <i className="fa-solid fa-circle-exclamation"></i></span>
+        </div>
+        <div className={`${Style.container} ${emailMessage === true ? `${Style.show}` : ''}`}>
+          <input ref={emailRef} type="email" name="email" id="email" className={Style.form_input} placeholder="Email Address" />
+          <span>Can´t be empty <i className="fa-solid fa-circle-exclamation"></i></span>
+        </div>
+        <div className={`${Style.container} ${phoneMessage === true ? `${Style.show}` : ''}`}>
+          <input ref={phoneRef} type="tel" name="tel" id="tel" className={Style.form_input} placeholder="Phone"/>
+          <span>Can´t be empty <i className="fa-solid fa-circle-exclamation"></i></span>
+        </div>
+        <textarea ref={messageRef} name="message" id="message" cols={40} rows={4} placeholder="Your Message"></textarea>
         <button type="submit">submit</button>
       </form>
     </section>
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import { NextPage } from "next";
 import Style from "../styles/Contact.module.scss"
 import { Header } from "@/components/Header/Header";
 import ContactCard from "@/components/ContactCard/ContactCard";
 import LocationCards from "@/components/LocationCards/LocationCards";
 import Footer from "@/components/Footer/Footer";
+import Head from "next/head";
 
 const Contact: NextPage = () => {
   return (
     <>
+      <Head>
+        <script src="https://kit.fontawesome.com/3b8b94a692.js" crossOrigin="anonymous"></script>
+      </Head>
+      
       <main className={Style.container}>
         <Header />
 
