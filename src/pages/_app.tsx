ebf489e9403e13dd2931835242cfcb7460e7ad39
fenmow <lucas170105@outlook.com>
2023-12-12 01:22:58 -0300

    bruh

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/Globals.scss'
 import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 import type { AppProps } from 'next/app'
 
