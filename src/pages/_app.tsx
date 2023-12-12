96ecc9803a7075e686925c607842a06f587f6ab1
fenmow <lucas170105@outlook.com>
2023-12-11 23:08:56 -0300

    Installed Font Awesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import '../styles/Globals.scss'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
